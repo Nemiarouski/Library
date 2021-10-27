7510ebd08257f40d22b53d220c90eed728d83624
Peter Nemiarouski <nemiarouskipetr@gmail.com>
2021-10-27 16:14:03 +0300

    Rename package dao to repository (with file names).

--- src/main/java/com/example/library/repository/BookRepository.java
+++ src/main/java/com/example/library/repository/BookRepository.java
@@ -1,10 +1,10 @@
-package com.example.library.dao;
+package com.example.library.repository;
 
 import com.example.library.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
-public interface BookDao extends JpaRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, Long> {
     Book findByName(String name);
     List<Book> findByAuthor(String author);
     List<Book> findByYear(String year);
--- src/main/java/com/example/library/repository/ReaderRepository.java
+++ src/main/java/com/example/library/repository/ReaderRepository.java
@@ -1,10 +1,10 @@
-package com.example.library.dao;
+package com.example.library.repository;
 
 import com.example.library.model.Reader;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
-public interface ReaderDao extends JpaRepository<Reader, Long> {
+public interface ReaderRepository extends JpaRepository<Reader, Long> {
     List<Reader> findByName(String name);
     List<Reader> findBySurname(String surname);
 }
