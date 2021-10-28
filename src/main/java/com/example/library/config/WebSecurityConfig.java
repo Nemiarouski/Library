87c80e4a2b9cf883dbcbc2e308c851ac8bc1aba2
Peter Nemiarouski <nemiarouskipetr@gmail.com>
2021-10-28 11:03:52 +0300

    Update URLs and access management.

--- src/main/java/com/example/library/config/WebSecurityConfig.java
+++ src/main/java/com/example/library/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.library.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -8,30 +9,36 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private DataSource dataSource;
+
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("Peter")
-                .password("pass")
-                .roles("ADMIN")
-                .and()
-                .withUser("Vataliy")
-                .password("mega")
-                .roles("USER");
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery(
+                        "select login, password, 'true' from readers where login=?")
+                .authoritiesByUsernameQuery(
+                        "select login, authority from readers where login=?");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
         http.authorizeRequests()
-                .antMatchers("/admin").hasRole("ADMIN")
-                .antMatchers("/user").hasAnyRole("USER", "ADMIN")
-                .antMatchers("/hello").permitAll()
-                .antMatchers("/").permitAll()
+                .antMatchers("/readers/**").hasAnyRole("READER", "ADMIN")
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/**").permitAll()
                 .and().formLogin();
     }
 
