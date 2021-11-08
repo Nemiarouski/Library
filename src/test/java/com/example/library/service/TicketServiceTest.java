9c8e31065307bfaa6f40e637cd037258291c8127
Peter Nemiarouski <nemiarouskipetr@gmail.com>
2021-11-08 11:43:13 +0300

    Add TicketServiceTest.

--- src/test/java/com/example/library/service/TicketServiceTest.java
+++ src/test/java/com/example/library/service/TicketServiceTest.java
@@ -7,66 +7,90 @@ import com.example.library.repository.BookRepository;
 import com.example.library.repository.ReaderRepository;
 import com.example.library.repository.TicketRepository;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+//@ExtendWith(MockitoExtension.class)
+//@RunWith(MockitoJUnitRunner.class)
 class TicketServiceTest {
-
-    @Mock
+/*    @Mock
     TicketRepository ticketRepository;
     @Mock
     BookRepository bookRepository;
     @Mock
     ReaderRepository readerRepository;
     @InjectMocks
-    TicketService ticketService = new TicketService(ticketRepository, bookRepository, readerRepository);
+    TicketService ticketService;*/
+
+    TicketRepository ticketRepository = mock(TicketRepository.class);
+    BookRepository bookRepository = mock(BookRepository.class);
+    ReaderRepository readerRepository = mock(ReaderRepository.class);
+
+    TicketService ticketService = new TicketService(ticketRepository,
+            bookRepository,
+            readerRepository);
 
     @Test
     void getBook() {
         Reader reader = new Reader("Michel", "Jackson", "king", "777", true, "ROLE_READER");
         Book book = new Book("Triumphal Arch", "Erich Maria Remarque", "1945", 6L);
 
-        when(readerRepository.findById(1L)).thenReturn(java.util.Optional.of(reader));
-        when(bookRepository.findById(1L)).thenReturn(java.util.Optional.of(book));
+        when(readerRepository.findById(1L)).thenReturn(Optional.of(reader));
+        when(bookRepository.findById(1L)).thenReturn(Optional.of(book));
 
         ticketService.getBook(1L, 1L);
-        assertEquals(5, bookRepository.getById(1L).getAmount());
+        assertEquals(5, book.getAmount());
     }
 
     @Test
     void returnBook() {
         Reader reader = new Reader("Michel", "Jackson", "king", "777", true, "ROLE_READER");
-        Book book1 = new Book("Triumphal Arch", "Erich Maria Remarque", "1945", 5L);
-        Book book2 = new Book("Arch", "Erich", "1999", 20L);
-        List<Ticket> tickets = Arrays.asList(new Ticket(reader, book1, "2021-04-25", ""),
-                                            new Ticket(reader, book2, "2021-04-25", ""));
+        Book book = new Book("Triumphal Arch", "Erich Maria Remarque", "1945", 7L);
+        Ticket ticket = new Ticket(reader, book, "2021-04-07 14:22", "");
 
-        when(readerRepository.findById(2L)).thenReturn(java.util.Optional.of(reader));
-        when(bookRepository.findById(2L)).thenReturn(java.util.Optional.of(book1));
-        when(ticketRepository.findAll()).thenReturn(tickets);
+        when(bookRepository.findById(1L)).thenReturn(Optional.of(book));
+        when(ticketRepository.findByReaderIdAndBookIdAndDateToIsNull(1L, 1L)).thenReturn(Optional.of(ticket));
 
-        ticketService.getBook(2L, 2L);
-        assertEquals(4,book1.getAmount());
-
-        ticketService.returnBook(2L, 2L);
-        assertEquals(5, book1.getAmount());
-
+        ticketService.returnBook(1L, 1L);
+        assertEquals(8, book.getAmount());
     }
 
     @Test
-    void findAll() {
+    void getTickets() {
         Reader reader = new Reader("Michel", "Jackson", "king", "777", true, "ROLE_READER");
-        Book book1 = new Book("Triumphal Arch", "Erich Maria Remarque", "1945", 5L);
-        Book book2 = new Book("Arch", "Erich", "1999", 20L);
-        List<Ticket> tickets = Arrays.asList(new Ticket(reader, book1, "2021-04-25", ""),
-                new Ticket(reader, book2, "2021-04-25", ""));
+        Book book1 = new Book("Triumphal Arch", "Erich Maria Remarque", "1945", 7L);
+        Book book2 = new Book("Cloud Atlas", "David Mitchell", "2004", 3L);
+        Ticket ticket1 = new Ticket(reader, book1, "2020-06-11 14:13", "2020-07-04 12:24");
+        Ticket ticket2 = new Ticket(reader, book2, "2021-01-02 11:10", "");
+        List<Ticket> tickets = new ArrayList<>();
+        tickets.add(ticket1);
+        tickets.add(ticket2);
 
         when(ticketRepository.findAll()).thenReturn(tickets);
-        assertEquals(2, ticketService.findAll().size());
 
+        assertTrue(ticketService.getTickets().containsKey("Cloud Atlas"));
+        assertTrue(ticketService.getTickets().containsKey("Triumphal Arch"));
+    }
+
+    @Test
+    void getReaderBooks() {
+        Reader reader = new Reader("Michel", "Jackson", "king", "777", true, "ROLE_READER");
+        reader.setId(1L);
+        Book book1 = new Book("Triumphal Arch", "Erich Maria Remarque", "1945", 7L);
+        Book book2 = new Book("Cloud Atlas", "David Mitchell", "2004", 3L);
+        Ticket ticket1 = new Ticket(reader, book1, "2020-06-11 14:13", "2020-07-04 12:24");
+        Ticket ticket2 = new Ticket(reader, book2, "2021-01-02 11:10", "");
+        List<Ticket> tickets = new ArrayList<>();
+        tickets.add(ticket1);
+        tickets.add(ticket2);
+
+        when(ticketRepository.findAll()).thenReturn(tickets);
+
+        assertEquals("Triumphal Arch", ticketService.getReaderBooks(1L).get(0).getBookName());
     }
 }
