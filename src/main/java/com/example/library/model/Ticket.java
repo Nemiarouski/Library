17e890ea54e6c89d3227bd90cfb0872960a62f13
Peter Nemiarouski <nemiarouskipetr@gmail.com>
2021-11-19 12:47:11 +0300

    Update validation annotation.

--- src/main/java/com/example/library/model/Ticket.java
+++ src/main/java/com/example/library/model/Ticket.java
@@ -4,8 +4,6 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
